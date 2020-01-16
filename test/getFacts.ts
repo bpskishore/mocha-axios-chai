7f9c289c99c274fef0b13056c92500eccb128e31
bpskishore <bpskishore@gmail.com>
2020-01-16 17:12:34 -0500

    test

--- test/getFacts.ts
+++ test/getFacts.ts
@@ -9,4 +9,11 @@ describe('Cat Facts', () => {
             expect(value.text).not.to.be.null;
         })
     })
+
+    it('getFacts', async () => {
+        let resp = await getFacts();
+        resp.data.all.forEach((value: any) => {
+            expect(value.text).not.to.be.null;
+        })
+    })
 })
